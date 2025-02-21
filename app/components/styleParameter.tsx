cd68ff251fbaa62dbf813b0c4d5d1522079dc7af
Xavi <khellendros1@gmail.com>
2025-02-21 22:28:42 +0100

    Added half points

--- app/components/styleParameter.tsx
+++ app/components/styleParameter.tsx
@@ -23,7 +23,7 @@ const StyleParameter: React.FC<StyleParameterProps> = ({ label, isSog, isPen, in
     const max = isPen ? 20 : 3;
 
     const incrementValue = (value: number, setValue: React.Dispatch<React.SetStateAction<number>>, max: number) => {
-        setValue((value + 1) % (max + 1));
+        setValue((value + 0.5) % (max + 1));
     };
 
     const checkColor = () => {
@@ -47,8 +47,8 @@ const StyleParameter: React.FC<StyleParameterProps> = ({ label, isSog, isPen, in
                         setLeftTotalIncrement(prev => prev + 0.1);
                     }
                     else {
-                        incrementLeftValue(leftValue === max ? -(max * 0.2) : 0.2);
-                        setLeftTotalIncrement(prev => prev + 0.2);
+                        incrementLeftValue(leftValue === max ? -(max * 0.1) : 0.1);
+                        setLeftTotalIncrement(prev => prev + 0.1);
                     }
                 }}
             >
@@ -68,8 +68,8 @@ const StyleParameter: React.FC<StyleParameterProps> = ({ label, isSog, isPen, in
                         setRightTotalIncrement(prev => prev + 0.1);
                     }
                     else {
-                        incrementRightValue(rightValue === max ? -(max * 0.2) : 0.2);
-                        setRightTotalIncrement(prev => prev + 0.2);
+                        incrementRightValue(rightValue === max ? -(max * 0.1) : 0.1);
+                        setRightTotalIncrement(prev => prev + 0.1);
                     }
                 }}
             >
